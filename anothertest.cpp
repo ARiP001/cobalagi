1a2e786afb4ef845e34baa45c789c9e61d3e0ff4
ARiP001 <ariffathurrahman0@gmail.com>
2025-02-14 01:02:28 +0700

    init

---
+++ anothertest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a, b;
+    int hasil;
+
+    cout << "masukkan nilai a : ";cin >> a;
+    cout << "masukkan nilai b : ";cin >> b;
+    hasil = a + b;
+    cout << "Hasilnya adalah : "<< hasil << endl; 
+
+    return 0;
+}
